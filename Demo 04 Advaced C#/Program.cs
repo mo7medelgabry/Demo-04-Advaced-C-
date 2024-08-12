48d68c205043bc271bd81b4a6e4bbab6bb2504bb
mo7medelgabry <mo7medelgabry@gmail.com>
2024-08-13 00:17:16 +0300

    Add project files.

---
+++ Demo 04 Advaced C#/Program.cs
@@ -0,0 +1,146 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Demo_04_Advaced_C_
+{
+    public delegate int StringFuncDelegate(string name);
+    //new Delegate 
+    public delegate bool CheckNum(int A);
+    internal class Program
+    { 
+       //static List<int> FindOddNumbers(List<int> list)
+       // {
+       //     List<int> Result = new List<int>();
+       //     if (list != null)
+       //     {
+       //         for(int i = 0; i < list.Count; i++)
+       //         {
+       //             if (list[i] %2 !=0)
+       //             {
+       //                 Result.Add(list[i]);
+       //             }
+       //         }
+       //     }
+
+       static List<int> FindNumbers(List<int> list,CheckNum reference)
+        {
+            List<int> Result = new List<int>();
+            if (list is not null)
+            {
+                for(int i = 0; i < list.Count; i++)
+                {
+                    if (reference.Invoke(list[i]))
+                    {
+                        Result.Add(list[i]);
+                    }
+                }
+            }
+            
+            
+            return Result;
+        }
+
+        static void Main(string[] args)
+        {
+           StringFuncDelegate X = StringsFunction.GetCountUpperChar;
+            //X= StringsFunction.GetCountLowerChar;
+            X += StringsFunction.GetCountLowerChar;
+            int Result = X.Invoke(name:"HEllo");  
+
+            //Console.WriteLine(Result);
+
+            int[] arr = { 5, 7, 9, 12, 0 };
+            ConditionFancDelegate RefAsc = SortingFunctions.Gtr;
+            ConditionFancDelegate RefDes = SortingFunctions.Less;
+
+            SortingAlgorithms.BubbleSort(arr,RefDes);
+           //SortingAlgorithms.BubbleSortDes(arr);
+
+            foreach (int i in arr)
+            {
+                Console.Write(i+" ");
+            }
+            Console.WriteLine("\n=====================================");
+            string[] Names = { "Mahmoud", "Ahmed", "Omar", "Mariam", "Aya" };
+            SortingAlgorithms.BubbleSort(Names, SortingFunctions.Gtr);
+
+           foreach (string i  in Names)
+            {
+                Console.Write(i+" ");
+            }
+            Console.WriteLine("\n=====================================");
+
+            // List<int> list = Enumerable.Range(start:1 , count:100 ).ToList();
+            //List<int> OddNumbers =  FindOddNumbers(list);
+            // foreach (int i in OddNumbers)
+            // {
+            //     Console.Write(i+" ");
+            // }
+            List<int> list = Enumerable.Range(start: 1, count: 100).ToList();
+            List<int> OddNumbers = FindNumbers(list,ConditionsFunctions.CheckOdd);
+            foreach (int i in OddNumbers)
+            {
+                Console.Write(i + " ");
+            }
+
+            Console.WriteLine("\n=====================================");
+
+            //Predicate<int> predicate = ConditionsFunctions.CheckEven;
+            //Func<int,bool> func = ConditionsFunctions.CheckOdd;
+              
+
+
+
+
+        }
+    }
+
+    class StringsFunction
+    {
+        public static int GetCountUpperChar(string Word)
+        { 
+            int Count = 0;
+            if(!string.IsNullOrEmpty(Word))
+            { 
+                for(int i = 0; i < Word.Length; i++)
+                {
+                    if (Char.IsUpper(Word[i]))
+                    {
+                        Count++;
+
+                    }
+                }
+
+            }
+
+            return Count;
+        }public static int GetCountLowerChar(string Word)
+        { 
+            int Count = 0;
+            if(!string.IsNullOrEmpty(Word))
+            { 
+                for(int i = 0; i < Word.Length; i++)
+                {
+                    if (Char.IsLower(Word[i]))
+                    {
+                        Count++;
+
+                    }
+                }
+
+            }
+
+            return Count;
+        }
+    }
+
+    class ConditionsFunctions
+    {
+        public static bool CheckOdd(int x) { return x % 2 != 0; }
+        public static bool CheckEven(int x) { return x % 2 == 0; }
+        public static bool CheckDivBy4(int x) { return x % 4 == 0; }
+
+
+    }
+}
---
+++ Demo 04 Advaced C#/SortingAlgorithms.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_04_Advaced_C_
+{
+    // delegate
+    public delegate bool ConditionFancDelegate(int A, int B);
+    public delegate bool ConditionFancDelegatev02(string A, string B);
+    internal class SortingAlgorithms
+    {
+        public static void BubbleSort(int[] Arr , ConditionFancDelegate  referance)
+        {
+            if (Arr is not null)
+            {
+                for (int i = 0; i < Arr.Length; i++)
+                {
+                    for(int j = 0; j< Arr.Length-i-1; j++)
+                    {
+                        if (referance.Invoke(Arr[j], Arr[j + 1]))
+                        {
+                            Swap(ref Arr[j], ref Arr[j+1]);
+                        }
+                    }
+                }
+            }
+        } public static void BubbleSort(string[] Arr , ConditionFancDelegatev02  referance)
+        {
+            if (Arr is not null)
+            {
+                for (int i = 0; i < Arr.Length; i++)
+                {
+                    for(int j = 0; j< Arr.Length-i-1; j++)
+                    {
+                        if (referance.Invoke(Arr[j], Arr[j + 1]))
+                        {
+                            Swap(ref Arr[j], ref Arr[j+1]);
+                        }
+                    }
+                }
+            }
+        }   
+        
+
+        // public static void BubbleSortDes(int[] Arr)
+        //{
+        //    if (Arr is not null)
+        //    {
+        //        for (int i = 0; i < Arr.Length; i++)
+        //        {
+        //            for(int j = 0; j< Arr.Length-i-1; j++)
+        //            {
+        //                if (SortingFunctions.Less(Arr[j], Arr[j+1]))
+        //                {
+        //                    Swap(ref Arr[j], ref Arr[j+1]);
+        //                }
+        //            }
+        //        }
+        //    }
+        //}
+
+        //public static void BubbleSort(int[] Arr)
+        //{
+        //    if (Arr is not null)
+        //    {
+        //        for (int i = 0; i < Arr.Length; i++)
+        //        {
+        //            for(int j = 0; j< Arr.Length-i-1; j++)
+        //            {
+        //                if(Arr[j] > Arr[j + 1])
+        //                {
+        //                    Swap(ref Arr[j], ref Arr[j+1]);
+        //                }
+        //            }
+        //        }
+        //    }
+        //} 
+
+        //public static void BubbleSortDes(int[] Arr)
+        //{
+        //    if (Arr is not null)
+        //    {
+        //        for (int i = 0; i < Arr.Length; i++)
+        //        {
+        //            for(int j = 0; j< Arr.Length-i-1; j++)
+        //            {
+        //                if(Arr[j] < Arr[j + 1])
+        //                {
+        //                    Swap(ref Arr[j], ref Arr[j+1]);
+        //                }
+        //            }
+        //        }
+        //    }
+        //}
+
+        public static void Swap(ref int x, ref int y)
+        {
+            int temp = x;
+            x=y; 
+            y=temp;
+
+        } public static void Swap(ref string x, ref string y)
+        {
+            string temp = x;
+            x=y; 
+            y=temp;
+
+        }
+
+
+    } 
+
+    class SortingFunctions
+    {
+        public static bool Gtr(string L, string R)
+        {
+            return (L.Length> R.Length);
+        } 
+        public static bool Less(string L, string R)
+        {
+            return (L.Length< R.Length);
+        }public static bool Gtr(int L, int R)
+        {
+            return (L> R);
+        } 
+        public static bool Less(int L, int R)
+        {
+            return (L< R);
+        }
+    }
+}
